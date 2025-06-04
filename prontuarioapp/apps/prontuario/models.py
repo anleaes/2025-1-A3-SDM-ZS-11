26cdc41de92aecb6098380e9507a9bbf1cb341c5
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-04 20:52:52 -0300

    Adequação finalizada e correção de bugs

--- prontuarioapp/apps/prontuario/models.py
+++ prontuarioapp/apps/prontuario/models.py
@@ -6,11 +6,11 @@ from receita.models import Receita
 
 
 class Prontuario(models.Model):
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='prontuarios', default=None)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='prontuarios', default=1)
     exames = models.ManyToManyField(Exame, blank=True, related_name='Exames')
     conteudo = models.TextField(blank=True, null=True)
-    receita = models.ForeignKey(Receita, on_delete=models.CASCADE, related_name='receitas', default=None)
-    
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE, related_name='receitas', default=1)
+
     class Meta:
         verbose_name = 'Prontuário'
         verbose_name_plural = 'Prontuários'
