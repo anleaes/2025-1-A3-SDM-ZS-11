f2315dd3e3b752beff7b8fba1865db784da31b2a
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-04 09:31:14 -0300

    Criação das urls de pessoa

---
+++ prontuarioapp/apps/pessoa/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = 'pessoa'
+
+router = routers.DefaultRouter()
+router.register('', views.PessoaViewSet, basename='pessoa')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
