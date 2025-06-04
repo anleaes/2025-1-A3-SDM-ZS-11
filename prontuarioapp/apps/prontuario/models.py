b86d7f33e0af38a37013cebc1a2f3fe797164aae
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-04 20:10:52 -0300

    Criação do models sem receitas pois ainda não foi criado a classe receita

--- prontuarioapp/apps/prontuario/models.py
+++ prontuarioapp/apps/prontuario/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from exame.models import Exame
 
-# Create your models here.
+
+class Prontuario(models.Model):
+    
+    exames = models.ManyToManyField(Exame, blank=True, related_name='Exames')
+    conteudo = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name = 'Prontuário'
+        verbose_name_plural = 'Prontuários'
+        ordering =['id']
+        
+
+    def __str__(self):
+        return self.nome
