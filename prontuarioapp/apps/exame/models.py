f15ebce0d09e3c3cbccd4ddfef5b5b171571158f
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-04 19:47:14 -0300

    Criação do models

--- prontuarioapp/apps/exame/models.py
+++ prontuarioapp/apps/exame/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Exame(models.Model):
+    nomeExame = models.CharField(max_length=100, verbose_name='Nome do Exame')
+    dataRealizacao = models.DateField(verbose_name='Data de Realização')
+    resultado = models.TextField(verbose_name='Resultado do Exame')
+    
+    class Meta:
+        verbose_name = 'Exame'
+        verbose_name_plural = 'Exame'
+        ordering =['id']
+        
+
+    def __str__(self):
+        return self.nome
