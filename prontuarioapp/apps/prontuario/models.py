9753a5fba7e050716948ca08dbb4ac8ef7625f73
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-04 20:28:14 -0300

    Finalizada a classe prontuario

--- prontuarioapp/apps/prontuario/models.py
+++ prontuarioapp/apps/prontuario/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from exame.models import Exame
+from paciente.models import Paciente
+
 
 
 class Prontuario(models.Model):
-    
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='prontuarios', default=None)
     exames = models.ManyToManyField(Exame, blank=True, related_name='Exames')
     conteudo = models.TextField(blank=True, null=True)
     
