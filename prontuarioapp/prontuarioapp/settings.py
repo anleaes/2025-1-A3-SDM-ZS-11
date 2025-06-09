434b2f1028f45ee468a9bf8d21a73e04a5f11769
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-06-09 08:51:46 -0300

    Instalação e adequação do settings.py

--- prontuarioapp/prontuarioapp/settings.py
+++ prontuarioapp/prontuarioapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'pessoa.apps.PessoaConfig',
     'paciente.apps.PacienteConfig',
     'profissionalSaude.apps.ProfissionalsaudeConfig',
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'prontuarioapp.urls'
@@ -135,3 +137,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
